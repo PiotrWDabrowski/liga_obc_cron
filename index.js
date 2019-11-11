5987327e179a1ff554739dd81c8f2b3639ee3d81
Piotr Dąbrowski <dabrowski.p.w@gmail.com>
2019-11-11 20:19:08 +0100

    Trying heroku display fix

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const request = require('request');
 
 const source = 'https://www.oliviacentre.com/olivia-sports/olivia-sports-2019-2020/';
 
-return request({
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+request({
     method: 'GET',
     url: source
 }, (err, res, body) => {
