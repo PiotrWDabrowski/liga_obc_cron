47862f524de2adeef7340e557efc6158657a7886
Piotr Dąbrowski <dabrowski.p.w@gmail.com>
2019-11-15 00:00:01 +0100

    Added firebase integration

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const cheerio = require('cheerio');
+const express = require('express');
+const admin = require('firebase-admin');
+const moment = require('moment')();
 const request = require('request');
 
 const source = 'https://www.oliviacentre.com/olivia-sports/olivia-sports-2019-2020/';
 
-var express = require('express');
 var app = express();
-var moment = require('moment')();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    getMatchDaySource(response);
+    processMatchDaysWithCron(response);
 });
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 });
 
-function getMatchDaySource(response) {
-    request({
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://obc-liga.firebaseio.com/'
+});
+
+function processMatchDaysWithCron(response) {
+    getMatchDaySource((matchDaySource) => {
+        sendMatchDaySource(matchDaySource, response);
+    });
+}
+
+function sendMatchDaySource(matchDaySource) {
+    admin.database().ref('matchdaysSource').set(matchDaySource);
+}
+
+function getMatchDaySource(matchDaySourcePromise, response) {
+    return request({
         method: 'GET',
         url: source
     }, (err, res, body) => {
@@ -30,20 +46,11 @@ function getMatchDaySource(response) {
 
         let timeHeader = moment.format('MMMM Do YYYY, h:mm:ss a');
         let matchdaysSource = $('tbody').html();
+        matchDaySourcePromise(matchdaysSource);
         let responseCode = `${timeHeader} <br /> ${matchdaysSource}`;
-        request.post('https://us-central1-obc-liga.cloudfunctions.net/receiveMatchdays', {
-            json: {
-                matchdays: matchdaysSource
-            }
-        }, (error, res, body) => {
-            if (error) {
-                console.error(error)
-                return
-            }
-            console.log(`statusCode: ${res.statusCode}`)
-        })
         if (response) {
             response.send(responseCode);
         }
+
     });
 }
