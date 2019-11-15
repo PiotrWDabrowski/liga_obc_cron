b630a0a015da79aec6a14bc54bbd7959ae9c3079
Piotr Dąbrowski <dabrowski.p.w@gmail.com>
2019-11-15 22:55:41 +0100

    Operating single matches

--- index.js
+++ index.js
@@ -41,17 +41,34 @@ function getMatchDaySource(matchDaySourcePromise, response) {
     }, (err, res, body) => {
 
         if (err) return console.error(err);
+        let $ = cheerio.load(body);
+        var responseCode = [];
 
-        let $ = cheerio.load(body, {
-            xml: {
-                normalizeWhitespace: true,
+        $('tbody').find('tr').each(function (i, trElement) {
+            var match = [];
+            $(this).find('td').each(function (j, tdElement) {
+                match.push($(this).text());
+            });
+
+            // Is Matchday Title
+            if (match.length == 1) {
+                let line = match.join("");
+                console.log(line);
+                responseCode.push("<b>" + line + "</b>")
+            }
+            // Is Single match line
+            if (match.length == 7) {
+                let line = match.join(" ");
+                console.log(line);
+                responseCode.push(line);
             }
         });
+
 
         let timeHeader = moment.format('MMMM Do YYYY, h:mm:ss a');
         let matchdaysSource = $('tbody').html();
         matchDaySourcePromise(matchdaysSource);
-        let responseCode = `${timeHeader} <br /> ${matchdaysSource}`;
+        responseCode = `${timeHeader} <br /> ${responseCode}`;
         if (response) {
             response.send(responseCode);
         }
